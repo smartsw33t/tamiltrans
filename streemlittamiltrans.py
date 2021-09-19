8b3f82e71a43022486b69c2f719f13e86fd93ab1
smartsw33t <79314753+smartsw33t@users.noreply.github.com>
2021-09-19 11:16:27 +0530

    Update streemlittamiltrans.py

--- streemlittamiltrans.py
+++ streemlittamiltrans.py
@@ -1,23 +1,3 @@
-pip install google
-pip install google-api-core
-pip install --upgrade google-api-python-client
-pip install --upgrade google-cloud-translate
-pip install google-cloud
-pip install google-cloud-vision
-pip install google.cloud.bigquery
-pip install google.cloud.storage
-pip install protobuf
-pip install google-cloud-translate
 
-
-
-
-from google_trans_new import google_translator
-import streamlit as st
-translator = google_translator()
-st.title("மொழிபெயர்ப்புக் கருவி ")
-text = st.text_input("ஆங்கிலத்தில் டைப் செய்யுங்கள்")
-translate = translator.translate(text, lang_tgt='ta')
-st.write(translate)
 
 
